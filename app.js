d1d1f0e2558a33b42f694095f3511313998a2877
Anirban <anirbanghatak92@gmail.com>
2020-07-05 23:20:45 +0530

    Procfile

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const http = require("http");
 const routes = require("./routes");
 const express = require("express");
 const bodyParser = require("body-parser");
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
