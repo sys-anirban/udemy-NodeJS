898e92366caffba8441cd3441c58f7c5f8773e7a
Anirban <anirbanghatak92@gmail.com>
2020-07-06 14:41:00 +0530

    Added controller

--- app.js
+++ app.js
@@ -1,21 +1,24 @@
-const http = require("http");
+const path = require("path");
+
 const express = require("express");
 const bodyParser = require("body-parser");
+
+const app = express();
+
+app.set("view engine", "ejs");
+app.set("views", "views");
+
 const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
 
-const port = process.env.PORT || 3000;
-const path = require("path");
-const app = express();
-
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-  res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
+  res.status(404).render("404", { pageTitle: "Page Not Found" });
 });
 
-app.listen(port);
+app.listen(3000);
