968336d65aa502bc8c2f24b10249e25b9252f1cf
Anirban <anirbanghatak92@gmail.com>
2020-07-06 15:27:50 +0530

    Check Process ENV

--- app.js
+++ app.js
@@ -1,22 +1,25 @@
 const path = require("path");
-
 const express = require("express");
 const bodyParser = require("body-parser");
 
 const app = express();
 const port = process.env.PORT || 3000;
+
 app.set("view engine", "ejs");
 app.set("views", "views");
-
-const adminRoutes = require("./routes/admin");
-const shopRoutes = require("./routes/shop");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
+require("dotenv").config();
+
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
-
+app.use("/process", (req, res, next) => {
+  res.send(`<p>ENV Variable ${process.env.search}</p>`);
+});
 app.use((req, res, next) => {
   res.status(404).render("404", { pageTitle: "Page Not Found" });
 });
